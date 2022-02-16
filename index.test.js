377908aac9a42c833e45e485256f0ff57deba558
Sergey Tarasov <tsm6446463@gmail.com>
2022-02-16 21:46:47 +0300

    grammar errors fixed

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const { search } = require('./index')
+const { search } = require('./index');
 
 describe('method filter works', () => {
     let arr = [1, false, "apple", "car", 4, 56, "shovel", true];
     let array2 = [1, 4, 56];
-    it('array "number" has 3 elements', () => {
+    it('array "array2" has 3 elements', () => {
         expect(search(arr)).toEqual(array2)
     });
     it('should be defined', () => {
@@ -14,36 +14,35 @@ describe('method filter works', () => {
         expect(search(arr)).not.toContain(null);
         expect(search(arr)).not.toContain(0);
         expect(search(arr)).not.toContain(false);
-    })
-})
+    });
+});
 
-const { getNum } = require('./index')
+const { getNum } = require('./index');
 
 describe('function is valid', () => {
     it('should be false if recieves string', () =>
         expect(getNum('someStr')).toEqual(false));
     it('should be false if recieves even number', () =>
         expect(getNum(4)).toBe(false));
-    it('should be fulfilled successfully then odd if recieved', () =>
+    it('should be fulfilled successfully then odd number recieved', () =>
         expect(getNum(3)).toBe(4));
 });
 
 
-const { multiply } = require('./index')
+const { multiply } = require('./index');
 
 describe('multiply function validation', () => {
     it('check correct result', () =>
         expect(multiply(10, 5)).toBe(50));
     it('check wrong result expected', () =>
         expect(multiply(2, 10)).not.toBe(30))
-})
+});
 
 
-const { repWord } = require('./index')
+const { repWord } = require('./index');
 
 describe('should perform as expected', () => {
     it('should be defined', () => {
-        let str = 'cool1,cool2,cool3,'
         expect(repWord('love', 3)).not.toBeUndefined();
         expect(repWord('love', 3)).not.toBeNull();
     });
@@ -51,5 +50,5 @@ describe('should perform as expected', () => {
         let str = "hate1,hate2,"
         expect(repWord('hate', 6)).toContain('hate5');
         expect(repWord('hate', 2)).toEqual(str);
-    })
-})
+    });
+});
